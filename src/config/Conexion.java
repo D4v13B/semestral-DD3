e38b0cb1d4442fb6f1a3f86f1fb2e4436111c394
MonchoLaptop <moncho021509@gmail.com>
2024-06-24 15:30:28 -0500

    second commit

--- src/config/Conexion.java
+++ src/config/Conexion.java
@@ -1,34 +1,23 @@
 package config;
 import java.sql.*;
 public class Conexion {
-    private String nombre_bd = "investigadores";
     private String usuario_bd = "root";
     private String password_bd = "monchillo24";
     private String url = "jdbc:sqlserver://localhost:1433;databaseName=investigadores;encrypt=false;trustServerCertificate=false";
 
-    private Connection cnn; //objeto que permite la conexión a la BD
-
-//    public Conexion(String nombre_bd, String usuario_bd, String password_bd){
-//        this.nombre_bd=nombre_bd;
-//        this.usuario_bd= usuario_bd;
-//        this.password_bd=password_bd;
-//    }
+    private Connection cnn;
 
     // Método de conexion
-    public Connection establecer_conexion() throws Exception {
+    public Connection establecerConexion() throws Exception {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             cnn = DriverManager.getConnection(url, usuario_bd, password_bd);
             return cnn;
         }
         catch (SQLException e){
-            throw new Exception ("\nPara el programador: "+e+
+            throw new Exception ("\n" + e +
                     "\n\nPara el usuario: Error... No se pudo establecer la conexion");
         }
-    }
-
-    public String getNombre_bd() {
-        return nombre_bd;
     }
 }
 
